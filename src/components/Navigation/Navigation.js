1879e48661f3e569c2d831e102f87826b0979d8e
Will <percoschat@gmail.com>
2019-07-23 21:07:22 -0700

    added navigation and logo components

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='pa3'>Sign Out</p>
+            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
     );
 }
