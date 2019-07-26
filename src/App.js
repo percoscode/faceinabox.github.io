bb0bcbb0fb7bba7b7e0a2f2fcf76058dc2da4426
Will <percoschat@gmail.com>
2019-07-25 20:29:12 -0700

    completed components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from 'react-particles-js'; 
 import Navigation from  './components/Navigation/Navigation';
 import Logo from  './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -6,9 +7,23 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 import 'tachyons';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
