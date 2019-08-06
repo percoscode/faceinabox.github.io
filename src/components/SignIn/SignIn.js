4763ac5be1754ce74c58334e7c60759ba89f776c
Will <percoschat@gmail.com>
2019-08-06 12:32:46 -0700

    cleaned up SignIn.js

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,10 +18,10 @@ const SignIn = ({ imageUrl, box }) => {
                         </div>
                         </fieldset>
                         <div className="">
-                        <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"></input>
+                            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"></input>
                         </div>
                         <div className="lh-copy mt3">
-                        <a href="#0" className="f6 link dim black db">Register</a>
+                            <a href="#0" className="f6 link dim black db">Register</a>
                         </div>
                     </form>
                 </main>
